toeverything/OctoBase

fix(codec): replace any::integer repr with i32 (#515)
* fix(codec): replace any::integer repr with i32

* fix: move i64 test to i32

* fix: fuzz ci

---------

Co-authored-by: DarkSky <25152247+darkskygit@users.noreply.github.com>
Co-authored-by: DarkSky <darksky2048@gmail.com>

libs/jwst-codec/benches/codec_benchmarks.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
-use jwst_codec::{read_var_i64, read_var_u64, write_var_i64, write_var_u64};
+use jwst_codec::{read_var_i32, read_var_u64, write_var_i32, write_var_u64};
 use lib0::decoding::{Cursor, Read};
 use lib0::encoding::Write;
 
@@ -84,52 +84,26 @@ fn codec(c: &mut Criterion) {
     }
 
     {
-        codec_group.bench_function("jwst encode var_int (64 bit)", |b| {
-            b.iter(|| {
-                let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
-                for i in 0..(BENCHMARK_SIZE as i64) {
-                    write_var_i64(&mut encoder, i).unwrap();
-                }
-            })
-        });
-        codec_group.bench_function("jwst decode var_int (64 bit)", |b| {
-            let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
-            for i in 0..(BENCHMARK_SIZE as i64) {
-                write_var_i64(&mut encoder, i).unwrap();
-            }
-
-            b.iter(|| {
-                let mut decoder = encoder.as_slice();
-                for i in 0..(BENCHMARK_SIZE as i64) {
-                    let (tail, num) = read_var_i64(decoder).unwrap();
-                    decoder = tail;
-                    assert_eq!(num, i);
-                }
-            })
-        });
-    }
-
-    {
         codec_group.bench_function("jwst encode var_int (32 bit)", |b| {
             b.iter(|| {
                 let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
                 for i in 0..(BENCHMARK_SIZE as i32) {
-                    write_var_i64(&mut encoder, i as i64).unwrap();
+                    write_var_i32(&mut encoder, i).unwrap();
                 }
             })
         });
         codec_group.bench_function("jwst decode var_int (32 bit)", |b| {
             let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
             for i in 0..(BENCHMARK_SIZE as i32) {
-                write_var_i64(&mut encoder, i as i64).unwrap();
+                write_var_i32(&mut encoder, i).unwrap();
             }
 
             b.iter(|| {
                 let mut decoder = encoder.as_slice();
                 for i in 0..(BENCHMARK_SIZE as i32) {
-                    let (tail, num) = read_var_i64(decoder).unwrap();
+                    let (tail, num) = read_var_i32(decoder).unwrap();
                     decoder = tail;
-                    assert_eq!(num as i32, i);
+                    assert_eq!(num, i);
                 }
             })
         });

libs/jwst-codec/fuzz/fuzz_targets/decode_bytes.rs
@@ -1,10 +1,10 @@
 #![no_main]
 
-use jwst_codec::{read_var_buffer, read_var_i64, read_var_string, read_var_u64};
+use jwst_codec::{read_var_buffer, read_var_i32, read_var_string, read_var_u64};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<u8>| {
-    let _ = read_var_i64(&data);
+    let _ = read_var_i32(&data);
     let _ = read_var_u64(&data);
     let _ = read_var_buffer(&data);
     let _ = read_var_string(&data);

libs/jwst-codec/fuzz/fuzz_targets/i32_decode.rs
@@ -1,0 +1,18 @@
+#![no_main]
+
+use jwst_codec::{read_var_i32, write_var_i32};
+use lib0::encoding::Write;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: Vec<i32>| {
+    for i in data {
+        let mut buf1 = Vec::new();
+        write_var_i32(&mut buf1, i).unwrap();
+
+        let mut buf2 = Vec::new();
+        buf2.write_var(i);
+
+        assert_eq!(read_var_i32(&buf1).unwrap().1, i);
+        assert_eq!(read_var_i32(&buf2).unwrap().1, i);
+    }
+});

libs/jwst-codec/fuzz/fuzz_targets/i32_encode.rs
@@ -1,19 +1,14 @@
 #![no_main]
 
-use jwst_codec::write_var_i64;
+use jwst_codec::write_var_i32;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: Vec<i64>| {
+fuzz_target!(|data: Vec<i32>| {
     use lib0::encoding::Write;
 
     for i in data {
         let mut buf1 = Vec::new();
-        write_var_i64(&mut buf1, i).unwrap();
-
-        // lib0 will crash at this point
-        if i == i64::MIN {
-            continue;
-        }
+        write_var_i32(&mut buf1, i).unwrap();
         let mut buf2 = Vec::new();
         buf2.write_var(i);
 

y-crdt/y-octo

packages/y-octo/benches/codec_benchmarks.rs
@@ -3,7 +3,7 @@ use lib0::{
     decoding::{Cursor, Read},
     encoding::Write,
 };
-use y_octo::{read_var_i64, read_var_u64, write_var_i64, write_var_u64};
+use y_octo::{read_var_i32, read_var_u64, write_var_i32, write_var_u64};
 
 const BENCHMARK_SIZE: u32 = 100000;
 
@@ -86,52 +86,26 @@ fn codec(c: &mut Criterion) {
     }
 
     {
-        codec_group.bench_function("jwst encode var_int (64 bit)", |b| {
-            b.iter(|| {
-                let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
-                for i in 0..(BENCHMARK_SIZE as i64) {
-                    write_var_i64(&mut encoder, i).unwrap();
-                }
-            })
-        });
-        codec_group.bench_function("jwst decode var_int (64 bit)", |b| {
-            let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
-            for i in 0..(BENCHMARK_SIZE as i64) {
-                write_var_i64(&mut encoder, i).unwrap();
-            }
-
-            b.iter(|| {
-                let mut decoder = encoder.as_slice();
-                for i in 0..(BENCHMARK_SIZE as i64) {
-                    let (tail, num) = read_var_i64(decoder).unwrap();
-                    decoder = tail;
-                    assert_eq!(num, i);
-                }
-            })
-        });
-    }
-
-    {
         codec_group.bench_function("jwst encode var_int (32 bit)", |b| {
             b.iter(|| {
                 let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
                 for i in 0..(BENCHMARK_SIZE as i32) {
-                    write_var_i64(&mut encoder, i as i64).unwrap();
+                    write_var_i32(&mut encoder, i).unwrap();
                 }
             })
         });
         codec_group.bench_function("jwst decode var_int (32 bit)", |b| {
             let mut encoder = Vec::with_capacity(BENCHMARK_SIZE as usize * 8);
             for i in 0..(BENCHMARK_SIZE as i32) {
-                write_var_i64(&mut encoder, i as i64).unwrap();
+                write_var_i32(&mut encoder, i).unwrap();
             }
 
             b.iter(|| {
                 let mut decoder = encoder.as_slice();
                 for i in 0..(BENCHMARK_SIZE as i32) {
-                    let (tail, num) = read_var_i64(decoder).unwrap();
+                    let (tail, num) = read_var_i32(decoder).unwrap();
                     decoder = tail;
-                    assert_eq!(num as i32, i);
+                    assert_eq!(num, i);
                 }
             })
         });

packages/y-octo/fuzz/fuzz_targets/decode_bytes.rs
@@ -1,10 +1,10 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use y_octo::{read_var_buffer, read_var_i64, read_var_string, read_var_u64};
+use y_octo::{read_var_buffer, read_var_i32, read_var_string, read_var_u64};
 
 fuzz_target!(|data: Vec<u8>| {
-    let _ = read_var_i64(&data);
+    let _ = read_var_i32(&data);
     let _ = read_var_u64(&data);
     let _ = read_var_buffer(&data);
     let _ = read_var_string(&data);

packages/y-octo/fuzz/fuzz_targets/i32_decode.rs
@@ -1,0 +1,18 @@
+#![no_main]
+
+use lib0::encoding::Write;
+use libfuzzer_sys::fuzz_target;
+use y_octo::{read_var_i32, write_var_i32};
+
+fuzz_target!(|data: Vec<i32>| {
+    for i in data {
+        let mut buf1 = Vec::new();
+        write_var_i32(&mut buf1, i).unwrap();
+
+        let mut buf2 = Vec::new();
+        buf2.write_var(i);
+
+        assert_eq!(read_var_i32(&buf1).unwrap().1, i);
+        assert_eq!(read_var_i32(&buf2).unwrap().1, i);
+    }
+});

packages/y-octo/fuzz/fuzz_targets/i32_encode.rs
@@ -1,19 +1,14 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use y_octo::write_var_i64;
+use y_octo::write_var_i32;
 
-fuzz_target!(|data: Vec<i64>| {
+fuzz_target!(|data: Vec<i32>| {
     use lib0::encoding::Write;
 
     for i in data {
         let mut buf1 = Vec::new();
-        write_var_i64(&mut buf1, i).unwrap();
-
-        // lib0 will crash at this point
-        if i == i64::MIN {
-            continue;
-        }
+        write_var_i32(&mut buf1, i).unwrap();
         let mut buf2 = Vec::new();
         buf2.write_var(i);
 