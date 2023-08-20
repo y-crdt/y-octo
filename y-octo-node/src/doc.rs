7a1c804eede33730467f009cf28844905fff4d31
DarkSky <darksky2048@gmail.com>
2023-08-20 18:42:58 +0800

    feat: init build

---
+++ y-octo-node/src/doc.rs
@@ -0,0 +1,26 @@
+use super::*;
+use y_octo::Doc as YDoc;
+
+#[napi]
+pub struct Doc {
+    doc: YDoc,
+}
+
+#[napi]
+impl Doc {
+    #[napi(constructor)]
+    pub fn new(client_id: Option<i64>) -> Self {
+        Self {
+            doc: if let Some(client_id) = client_id {
+                YDoc::with_client(client_id as u64)
+            } else {
+                YDoc::default()
+            },
+        }
+    }
+
+    #[napi(getter)]
+    pub fn client_id(&self) -> i64 {
+        self.doc.client() as i64
+    }
+}
--- y-octo-node/src/lib.rs
+++ y-octo-node/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use napi_derive::napi;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod doc;
